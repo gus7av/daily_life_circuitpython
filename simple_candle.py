a1cb594d368bd6a9ac84719ff49028d77784006e
gus7av <66468289+gus7av@users.noreply.github.com>
2022-01-09 19:28:48 +0100

    Add files via upload

---
+++ simple_candle.py
@@ -0,0 +1,12 @@
+import circuitpython_essentials as cp
+import time
+from random import randint
+import board
+
+led = cp.output(board.LED)
+value = 32767
+
+while True:
+    value += randint(max(-4, -value), min(4, 65535 - value))
+    led.value = value
+    time.sleep(0.01)
