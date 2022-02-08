13f621e8878389490343a5859237e28dbb3b8d6e
gus7av <66468289+gus7av@users.noreply.github.com>
2022-02-08 13:09:41 +0100

    Update led_candle.py

--- led_candle.py
+++ led_candle.py
@@ -4,12 +4,12 @@ import board
 from random import randint
 
 led = cp.pwm_output(board.LED)
-previous = 32767
-flicker = 8191
+previous = 0x8000
+flicker = 0x2000
 
 while True:
-    value = randint(flicker, 65535 - flicker)
-    step = min(255, max(-255, value - previous))
+    value = randint(flicker, 0xFFFF - flicker)
+    step = min(256, max(-256, value - previous))
     if step != 0:
         for i in range(previous, value, step):
             led.value = i + randint(-flicker, flicker)
