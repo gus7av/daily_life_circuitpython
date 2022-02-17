3c2ef41b2af6bfa5055ca2255751e4bf166713f7
gus7av <66468289+gus7av@users.noreply.github.com>
2022-02-17 08:44:00 +0100

    Update sequencer_drums.py

--- sequencer/sequencer_drums.py
+++ sequencer/sequencer_drums.py
@@ -1,49 +1,35 @@
 import circuitpython_essentials as cp
 import board
 import time
-import random
 
-gnd_pin = cp.output(board.D1)
 btn_pin = cp.input_pullup(board.D4)
-tone_pot = cp.analog_input(board.A6)
-speed_pot = cp.analog_input(board.A8)
-speaker = cp.tone_output(board.D7)
+tone_pot = cp.analog_input(board.A7)
+speaker = cp.tone_output(board.D5)
 
-gnd_pin.value = False
 tones = [196, 220, 247, 262, 294, 330, 370, 392]  # g-major
 stored_value = [0] * 16
+speed = cp.map(tone_pot.value, 0, 0xFFFF, 0.6, 0.1)
 
 while True:
     for f in range(16):
-        speed = round(cp.map(speed_pot.value, 0, 0xFFFF, 0.1, 1), 2)
         tone = round(cp.map(tone_pot.value, 0, 0xFFFF, 0, len(tones)-1))
-        
+
         # record button
         if not btn_pin.value:
             stored_value[f] = tone
-        
-        # bass-drum
-        if stored_value[f] == 0:
-            timer = time.monotonic()
-            for i in range(196, 98, -2):
+
+        # percussion tones on 0 and 7
+        if stored_value[f] == 0 or stored_value[f] == 7:
+            for i in range(tones[stored_value[f]], tones[stored_value[f]]-100, -10):
                 speaker.value = i
-                time.sleep(0.001)
+                time.sleep(speed/20)
             speaker.stop()
-            duration = time.monotonic() - timer
-            time.sleep(speed-duration)
-        
-        # snare-drum
-        elif stored_value[f] == 7:
-            timer = time.monotonic()
-            for i in range(1, 40):
-                speaker.value = 392 + random.randrange(-i, i)
-                time.sleep(0.001)
-            speaker.stop()
-            duration = time.monotonic() - timer
-            time.sleep(speed - duration)
-        
-        # tone
+            time.sleep(speed/2)
+
+        # synth tones on the rest
         else:
             speaker.value = tones[stored_value[f]]
-            time.sleep(speed)
+            for i in range(0x8000, 0, -0x800):
+                speaker.volume = i
+                time.sleep(speed/16)
             speaker.stop()
