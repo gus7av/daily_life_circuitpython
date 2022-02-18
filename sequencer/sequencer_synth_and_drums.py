14186294cfa1df66acb811376f68de4b4d33de42
gus7av <66468289+gus7av@users.noreply.github.com>
2022-02-18 13:29:37 +0100

    Update sequencer_synth_and_drums.py

--- sequencer/sequencer_synth_and_drums.py
+++ sequencer/sequencer_synth_and_drums.py
@@ -6,13 +6,20 @@ btn_pin = cp.input_pullup(board.D4)
 tone_pot = cp.analog_input(board.A7)
 speaker = cp.tone_output(board.D5)
 
-tones = [196, 220, 247, 262, 294, 330, 370, 392]  # g-major
+f_major = 174, 196, 220, 233, 262, 294, 330, 349
+d_minor = 294, 330, 349, 392, 440, 466, 523, 587
+c_major = 262, 294, 330, 349, 392, 440, 494, 523
+a_minor = 220, 247, 262, 294, 330, 349, 392, 440
+g_major = 196, 220, 247, 262, 294, 330, 370, 392
+e_minor = 165, 185, 196, 220, 247, 262, 294, 330
+
+tones = c_major
 stored_value = [0] * 16
 
 value = cp.map(tone_pot.value, 0, 0xFFFF, 0.5, 0)
-min_speed = 0.1
-speed = value + min_speed
-sustain = min_speed*0.5 + value*0.1
+min_duration = 0.1
+duration = value + min_duration
+sustain = min_duration*0.5 + value*0.1
 
 def glide():
     step = round((tones[stored_value[f]] - tones[stored_value[f-1]])/10)
@@ -20,23 +27,24 @@ def glide():
         for i in range(10):
             speaker.value = tones[stored_value[f-1]] + i*step
             time.sleep(sustain/10)
-        time.sleep(speed-sustain)
+        speaker.value = tones[stored_value[f]]
+        time.sleep(duration-sustain)
         speaker.stop()
     else:
         speaker.value = tones[stored_value[f]]
-        time.sleep(speed)
+        time.sleep(duration)
         speaker.stop()
- 
-def synth(): 
+
+def synth():
     speaker.value = tones[stored_value[f]]
     for i in range(0x8000, 0, -0x800):
         speaker.volume = i
-        time.sleep(speed/16)
+        time.sleep(duration/16)
     speaker.stop()
-    
+
 def basic():
     speaker.value = tones[stored_value[f]]
-    time.sleep(speed)
+    time.sleep(duration)
     speaker.stop()
 
 while True:
@@ -53,7 +61,7 @@ while True:
                 speaker.value = i
                 time.sleep(sustain/10)
             speaker.stop()
-            time.sleep(speed-sustain)
+            time.sleep(duration-sustain)
 
         # chose tone type for the rest
         else:
