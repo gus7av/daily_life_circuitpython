bf0b9e4b0462e318136663a49e7d5d4c5374c412
gus7av <66468289+gus7av@users.noreply.github.com>
2022-04-04 10:54:02 +0200

    Update sequencer_synth_and_drums.py

--- sequencer/sequencer_synth_and_drums.py
+++ sequencer/sequencer_synth_and_drums.py
@@ -2,9 +2,11 @@ import circuitpython_essentials as cp
 import board
 import time
 
-btn_pin = cp.input_pullup(board.D4)
-tone_pot = cp.analog_input(board.A7)
-speaker = cp.tone_output(board.D5)
+btn_pin = cp.DigtitalIn(board.D4)
+btn_pin.pull_up()
+
+tone_pot = cp.AnalogIn(board.A7)
+speaker = cp.ToneOut(board.D5)
 
 f_major = 174, 196, 220, 233, 262, 294, 330, 349
 d_minor = 294, 330, 349, 392, 440, 466, 523, 587
@@ -17,7 +19,7 @@ scale = c_major
 steps = 32
 stored_value = [0] * steps
 
-value = cp.map(tone_pot.value, 0, 0xFFFF, 0.5, 0)
+value = cp.map_range(tone_pot.value, 0, 0xFFFF, 0.5, 0)
 min_duration = 0.1
 duration = value + min_duration
 sustain = min_duration * 0.5 + value * 0.1
@@ -54,20 +56,19 @@ def basic():
 
 while True:
     for f in range(steps):
-        tone = round(cp.map(tone_pot.value, 0, 0xFFFF, 0, len(scale) - 1))
+        tone = round(cp.map_range(tone_pot.value, 0, 0xFFFF, 0, len(scale) - 1))
 
         # record button
         if not btn_pin.value:
             stored_value[f] = tone
-
         # percussion tones on 0 and 7
         if stored_value[f] == 0 or stored_value[f] == 7:
-            for i in range(scale[stored_value[f]], scale[stored_value[f]] - 100, -10):
-                speaker.value = i
+            step = round(scale[stored_value[f]] / -20)
+            for i in range(10):
+                speaker.value = scale[stored_value[f]] + i * step
                 time.sleep(sustain / 10)
             speaker.stop()
             time.sleep(duration - sustain)
-
         # chose tone type for the rest
         else:
             glide()
