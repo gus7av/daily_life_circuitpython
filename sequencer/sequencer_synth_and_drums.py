541dc77b01a13321b1be71b98960e63ec81d3964
gus7av <66468289+gus7av@users.noreply.github.com>
2022-04-05 09:30:31 +0200

    Update sequencer_synth_and_drums.py

--- sequencer/sequencer_synth_and_drums.py
+++ sequencer/sequencer_synth_and_drums.py
@@ -2,11 +2,21 @@ import circuitpython_essentials as cp
 import board
 import time
 
-btn_pin = cp.DigtitalIn(board.D4)
-btn_pin.pull_up()
+rec_pin = cp.DigitalIn(board.D6)
+rec_pin.pull_up()
 
-tone_pot = cp.AnalogIn(board.A7)
-speaker = cp.ToneOut(board.D5)
+del_pin = cp.DigitalIn(board.D0)
+del_pin.pull_up()
+
+btn_gnd = cp.DigitalOut(board.D4)
+btn_gnd.value = False
+
+tone_pot = cp.AnalogIn(board.D3)
+
+pot_pwr = cp.DigitalOut(board.D5)
+pot_pwr.value = True
+
+speaker = cp.ToneOut(board.D8)
 
 f_major = 174, 196, 220, 233, 262, 294, 330, 349
 d_minor = 294, 330, 349, 392, 440, 466, 523, 587
@@ -15,62 +25,56 @@ a_minor = 220, 247, 262, 294, 330, 349, 392, 440
 g_major = 196, 220, 247, 262, 294, 330, 370, 392
 e_minor = 165, 185, 196, 220, 247, 262, 294, 330
 scale = c_major
+previous_note = scale[0]
 
 steps = 32
-stored_value = [0] * steps
+bars = int(steps / 4)
+bar = [0, 8, 0, 8]
+stored_value = bar * bars
 
 value = cp.map_range(tone_pot.value, 0, 0xFFFF, 0.5, 0)
 min_duration = 0.1
 duration = value + min_duration
 sustain = min_duration * 0.5 + value * 0.1
 
-
-def glide():
-    step = round((scale[stored_value[f]] - scale[stored_value[f - 1]]) / 10)
-    if step != 0:
-        for i in range(10):
-            speaker.value = scale[stored_value[f - 1]] + i * step
-            time.sleep(sustain / 10)
-        speaker.value = scale[stored_value[f]]
-        time.sleep(duration - sustain)
-        speaker.stop()
-    else:
-        speaker.value = scale[stored_value[f]]
-        time.sleep(duration)
-        speaker.stop()
-
-
-def synth():
-    speaker.value = scale[stored_value[f]]
-    for i in range(0x8000, 0, -0x800):
-        speaker.volume = i
-        time.sleep(duration / 16)
-    speaker.stop()
-
-
-def basic():
-    speaker.value = scale[stored_value[f]]
-    time.sleep(duration)
-    speaker.stop()
-
-
 while True:
     for f in range(steps):
         tone = round(cp.map_range(tone_pot.value, 0, 0xFFFF, 0, len(scale) - 1))
 
+        # delete button
+        if not del_pin.value:
+            stored_value[f] = 8
+        
         # record button
-        if not btn_pin.value:
+        if not rec_pin.value:
             stored_value[f] = tone
+        
         # percussion tones on 0 and 7
         if stored_value[f] == 0 or stored_value[f] == 7:
-            step = round(scale[stored_value[f]] / -20)
-            for i in range(10):
-                speaker.value = scale[stored_value[f]] + i * step
+            for i in range(scale[stored_value[f]], scale[stored_value[f]] - 100, -10):
+                speaker.value = i
                 time.sleep(sustain / 10)
             speaker.stop()
             time.sleep(duration - sustain)
-        # chose tone type for the rest
+        
+        # silence on 8
+        elif stored_value[f] == 8:
+            speaker.stop()
+            time.sleep(duration)
+        
+        #glide from previous tone
         else:
-            glide()
-            # synth()
-            # basic()
+            note = scale[stored_value[f]]
+            step = round((note - previous_note) / 10)
+            if step != 0:
+                for i in range(10):
+                    speaker.value = previous_note + i * step
+                    time.sleep(sustain / 10)
+                speaker.value = scale[stored_value[f]]
+                time.sleep(duration - sustain)
+                speaker.stop()
+            else:
+                speaker.value = scale[stored_value[f]]
+                time.sleep(duration)
+                speaker.stop()
+            previous_note = scale[stored_value[f]]
